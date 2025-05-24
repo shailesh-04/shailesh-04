3038d2218aaac084f03590849a145fb4e0e56db9
shailesh-04 <shailesakoli@gmail.com>
2025-05-24 22:00:58 +0530

    done

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,16 @@
 "use strict";
-// src/index.ts
-document.addEventListener("DOMContentLoaded", () => {
-    const heading = document.getElementById("greeting");
-    if (heading) {
-        heading.textContent = "Hello from TypeScript!";
-    }
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const app = (0, express_1.default)();
+const PORT = 3000;
+app.use(express_1.default.static(path_1.default.join(__dirname, "../public")));
+app.get("/", (_req, res) => {
+    res.sendFile(path_1.default.join(__dirname, "../public/index.html"));
+});
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
-// src/index.ts
-
-document.addEventListener("DOMContentLoaded", () => {
-  const heading = document.getElementById("greeting");
-  if (heading) {
-    heading.textContent = "Hello from TypeScript!";
-  }
+import express from "express";
+import path from "path";
+const app = express();
+const PORT = 3000;
+app.use(express.static(path.join(__dirname, "../public")));
+app.get("/", (_req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
